79be71f625b55ff51e4ec9320dedbd9601137f73
Shane Feek <shane.feek@gmail.com>
2021-07-19 09:55:41 -0400

    Added Median and code refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![windows_subsystem = "windows"]
 use fltk::{app::*, button::*, dialog::*, frame::*, group::*, input::*, text::*, window::*};
 use rand::Rng;
+use std::cmp::Ordering;
+use std::f64;
 
 #[derive(Clone, Debug)]
 // Define a struct for the form fields
@@ -31,7 +33,7 @@ fn main() {
         100,
         737,
         530,
-        "Bootstrap Mean Difference & Spearman Calculator v2.71",
+        "Bootstrap Mean Difference & Spearman Calculator v2.8",
     );
 
     // Fill the form structure
@@ -117,12 +119,12 @@ fn calculate(p: &mut Parameters) {
     let mut a_v: Vec<f64> = csv_split(&p.data_a.buffer().unwrap().text());
     let mut b_v: Vec<f64> = csv_split(&p.data_b.buffer().unwrap().text());
 
-    if a_v.len() < 1 {
+    if a_v.is_empty() {
         p.data_a.buffer().unwrap().set_text("0.0");
         a_v.push(0.0);
     }
 
-    if b_v.len() < 1 {
+    if b_v.is_empty() {
         p.data_b.buffer().unwrap().set_text("0.0");
         b_v.push(0.0);
     }
@@ -135,7 +137,7 @@ fn calculate(p: &mut Parameters) {
             return;
         }
     };
-    if iterations < 1000 || iterations > 9999000 {
+    if !(1000..=9999000).contains(&iterations) {
         alert(368, 265, "Iteration Count Error");
         return;
     };
@@ -148,7 +150,7 @@ fn calculate(p: &mut Parameters) {
             return;
         }
     };
-    if confidence < 0.0 || confidence > 100.0 {
+    if !(0.0..=100.0).contains(&confidence) {
         alert(368, 265, "Confidence Level Error");
         return;
     };
@@ -202,7 +204,7 @@ fn calculate(p: &mut Parameters) {
             "Mean B: \t{}\n",
             &science_pretty_format(mean_b, 6)
         ));
-        out.push_str("\n");
+        out.push('\n');
         out.push_str(&format!("Low Diff: \t{}\n", &science_pretty_format(l, 6)));
         out.push_str(&format!(
             "Mean Diff: \t{}\n",
@@ -213,13 +215,11 @@ fn calculate(p: &mut Parameters) {
 
         if l <= 0.0 && u >= 0.0 {
             out.push_str("H0 = True \tA ≈ B\n");
+        } else if mean_a > mean_b {
+            out.push_str("H0 = False \tA > B\n");
         } else {
-            if mean_a > mean_b {
-                out.push_str("H0 = False \tA > B\n");
-            } else {
-                out.push_str("H0 = False \tA < B\n");
-            }
-        };
+            out.push_str("H0 = False \tA < B\n");
+        }
     } else {
         // One Tailed
         let z = z_from_cl(1.0 - clevel * 2.0);
@@ -235,7 +235,7 @@ fn calculate(p: &mut Parameters) {
             "Mean B: \t{}\n",
             &science_pretty_format(mean_b, 6)
         ));
-        out.push_str("\n");
+        out.push('\n');
 
         if mean_a > mean_b {
             out.push_str(&format!("Low Diff: \t{}\n", &science_pretty_format(l, 6)));
@@ -276,7 +276,7 @@ fn calculate(p: &mut Parameters) {
 
         out.push_str(&format!("SD A:    \t{}\n", &science_pretty_format(sd_a, 6)));
         out.push_str(&format!("SD B:    \t{}\n", &science_pretty_format(sd_b, 3)));
-        out.push_str("\n");
+        out.push('\n');
         out.push_str(&format!("Low Diff: \t{}\n", &science_pretty_format(l, 6)));
         out.push_str(&format!("SD Diff: \t{}\n", &science_pretty_format(sd_d, 6)));
         out.push_str(&format!("High Diff: \t{}\n", &science_pretty_format(u, 6)));
@@ -284,13 +284,11 @@ fn calculate(p: &mut Parameters) {
 
         if l <= 0.0 && u >= 0.0 {
             out.push_str("H0 = True \tA ≈ B\n");
+        } else if sd_a > sd_b {
+            out.push_str("H0 = False \tA > B\n");
         } else {
-            if sd_a > sd_b {
-                out.push_str("H0 = False \tA > B\n");
-            } else {
-                out.push_str("H0 = False \tA < B\n");
-            }
-        };
+            out.push_str("H0 = False \tA < B\n");
+        }
     } else {
         // One Tailed
         let z = z_from_cl(1.0 - clevel * 2.0);
@@ -300,7 +298,7 @@ fn calculate(p: &mut Parameters) {
 
         out.push_str(&format!("SD A:    \t{}\n", &science_pretty_format(sd_a, 6)));
         out.push_str(&format!("SD B:    \t{}\n", &science_pretty_format(sd_b, 6)));
-        out.push_str("\n");
+        out.push('\n');
 
         if sd_a > sd_b {
             out.push_str(&format!("Low Diff: \t{}\n", &science_pretty_format(l, 6)));
@@ -322,6 +320,18 @@ fn calculate(p: &mut Parameters) {
             }
         }
     }
+
+    out.push_str("\n************************************\n");
+
+    out.push_str(&format!(
+        "Median A:    \t{}\n",
+        &science_pretty_format(median(&a_v), 6)
+    ));
+
+    out.push_str(&format!(
+        "Median B:    \t{}\n",
+        &science_pretty_format(median(&b_v), 6)
+    ));
 
     out.push_str("\n************************************\n");
 
@@ -372,8 +382,8 @@ fn calculate(p: &mut Parameters) {
 
             let cstring = match r {
                 r if r == 0.0 => "None",
-                r if r == 1.0 => "Perfect Pos",
-                r if r == -1.0 => "Perfect Neg",
+                r if (r - 1.0).abs() < f64::EPSILON => "Perfect Pos",
+                r if (r - -1.0).abs() < f64::EPSILON => "Perfect Neg",
                 r if r > 0.0 && r < 0.3 => "Weak Pos",
                 r if r >= 0.3 && r < 0.7 => "Moderate Pos",
                 r if r >= 0.7 && r < 1.0 => "Strong Pos",
@@ -403,7 +413,7 @@ fn calculate(p: &mut Parameters) {
 }
 
 // Paired data
-fn paired_data(a_v: &Vec<f64>, b_v: &Vec<f64>, iterations: i32) -> Sdmeanresults {
+fn paired_data(a_v: &[f64], b_v: &[f64], iterations: i32) -> Sdmeanresults {
     let mut dmeans: Vec<f64> = Vec::new();
     let mut dsds: Vec<f64> = Vec::new();
     let mut tmp: Vec<f64> = Vec::new();
@@ -433,7 +443,7 @@ fn paired_data(a_v: &Vec<f64>, b_v: &Vec<f64>, iterations: i32) -> Sdmeanresults
 }
 
 // Unpaired data
-fn unpaired_data(a_v: &Vec<f64>, b_v: &Vec<f64>, iterations: i32) -> Sdmeanresults {
+fn unpaired_data(a_v: &[f64], b_v: &[f64], iterations: i32) -> Sdmeanresults {
     let mut dmeans: Vec<f64> = Vec::new();
     let mut ameans: Vec<f64> = Vec::new();
     let mut bmeans: Vec<f64> = Vec::new();
@@ -479,7 +489,7 @@ fn unpaired_data(a_v: &Vec<f64>, b_v: &Vec<f64>, iterations: i32) -> Sdmeanresul
 }
 
 // Convert CSV from the main windows to arrays of floats, also clean up stray whitespace
-fn csv_split(inp: &String) -> Vec<f64> {
+fn csv_split(inp: &str) -> Vec<f64> {
     let mut values: Vec<f64> = Vec::new();
 
     let clean_inp: String = inp
@@ -488,7 +498,7 @@ fn csv_split(inp: &String) -> Vec<f64> {
         .filter(|c| !c.is_whitespace())
         .collect();
 
-    let fields = clean_inp.split(",");
+    let fields = clean_inp.split(',');
 
     for f in fields {
         match f.parse::<f64>() {
@@ -501,24 +511,47 @@ fn csv_split(inp: &String) -> Vec<f64> {
 }
 
 // Calculate mean
-fn mean(vec: &Vec<f64>) -> f64 {
+fn mean(vec: &[f64]) -> f64 {
     let sum: f64 = Iterator::sum(vec.iter());
     sum / vec.len() as f64
 }
 
-// Calculate SD of a sample
-fn sd_sample(x: &Vec<f64>, mean: &f64) -> f64 {
-    let mut sd: f64 = 0.0;
-    let size: usize = x.len();
+// Calculate median
+fn median(vec: &[f64]) -> f64 {
+    let mut v = vec.to_owned();
 
-    for i in 0..size {
-        sd += (x[i] - mean).powf(2.0);
+    v.sort_by(cmp_f64);
+    v[vec.len() / 2]
+}
+
+// Comparison function for vec<64> sorting
+fn cmp_f64(a: &f64, b: &f64) -> Ordering {
+    if a.is_nan() {
+        return Ordering::Greater;
     }
-    (sd / (size - 1) as f64).sqrt()
+    if b.is_nan() {
+        return Ordering::Less;
+    }
+    if a < b {
+        return Ordering::Less;
+    } else if a > b {
+        return Ordering::Greater;
+    }
+    Ordering::Equal
+}
+
+// Calculate SD of a sample
+fn sd_sample(x: &[f64], mean: &f64) -> f64 {
+    let mut sd: f64 = 0.0;
+
+    for v in x.iter() {
+        sd += (v - mean).powf(2.0);
+    }
+    (sd / (x.len() - 1) as f64).sqrt()
 }
 
 // Calculate Skewness
-fn skewness(vec: &Vec<f64>, mean: &f64, sd: &f64) -> f64 {
+fn skewness(vec: &[f64], mean: &f64, sd: &f64) -> f64 {
     let sz: f64 = vec.len() as f64;
     let mut tmpsum: f64 = 0.0;
     let sdp = sd.powf(3.0);
@@ -531,7 +564,7 @@ fn skewness(vec: &Vec<f64>, mean: &f64, sd: &f64) -> f64 {
 }
 
 // Calculate Kurtosis
-fn kurtosis(vec: &Vec<f64>, mean: &f64, sd: &f64) -> f64 {
+fn kurtosis(vec: &[f64], mean: &f64, sd: &f64) -> f64 {
     let sz: f64 = vec.len() as f64;
     let mut tmpsum: f64 = 0.0;
     let sdp = sd.powf(4.0);
@@ -545,7 +578,7 @@ fn kurtosis(vec: &Vec<f64>, mean: &f64, sd: &f64) -> f64 {
 }
 
 // Rankify
-fn rankify(x: &Vec<f64>) -> Vec<f64> {
+fn rankify(x: &[f64]) -> Vec<f64> {
     let n = x.len();
     let mut rank: Vec<f64> = Vec::new();
 
@@ -557,7 +590,7 @@ fn rankify(x: &Vec<f64>) -> Vec<f64> {
             if x[j] < x[i] {
                 r += 1;
             }
-            if x[j] == x[i] {
+            if (x[j] - x[i]).abs() < f64::EPSILON {
                 s += 1;
             }
         }
@@ -566,7 +599,7 @@ fn rankify(x: &Vec<f64>) -> Vec<f64> {
             if x[j] < x[i] {
                 r += 1;
             }
-            if x[j] == x[i] {
+            if (x[j] - x[i]).abs() < f64::EPSILON {
                 s += 1;
             }
         }
@@ -609,8 +642,8 @@ fn l_gamma(x: f64) -> f64 {
     let mut denom: f64 = x + 1.0;
     let mut series: f64 = 1.000000000190015;
 
-    for i in 0..6 {
-        series += coef[i] / denom;
+    for v in &coef {
+        series += v / denom;
         denom += 1.0;
     }
 
@@ -640,11 +673,11 @@ fn p_from_t(ws: f64, dof: f64) -> f64 {
     let pp: f64;
     let indx: i32;
 
-    if value < 0.0 || 1.0 < value {
+    if !(0.0..=1.0).contains(&value) {
         return value;
     }
 
-    if value == 0.0 || value == 1.0 {
+    if (value - 0.0).abs() < f64::EPSILON || (value - 1.0).abs() < f64::EPSILON {
         return value;
     }
 
@@ -710,7 +743,7 @@ fn p_from_t(ws: f64, dof: f64) -> f64 {
 
 // Calculate P-Value from CI
 fn p_from_ci(l: f64, u: f64, m: f64, cl: f64) -> f64 {
-    let s: f64 = erf_inv(cl) * 1.414213562373095;
+    let s: f64 = erf_inv(cl) * f64::consts::SQRT_2;
     let se: f64 = (u - l) / (2.0 * s);
     let z: f64 = m / se;
 
@@ -725,7 +758,7 @@ fn erf_inv(x: f64) -> f64 {
     w = -((1.0 - x) * (1.0 + x)).ln();
 
     if w < 5.000000 {
-        w = w - 2.500000;
+        w -= 2.500000;
         p = 2.81022636e-08;
         p = 3.43273939e-07 + p * w;
         p = -3.5233877e-06 + p * w;
@@ -753,7 +786,7 @@ fn erf_inv(x: f64) -> f64 {
 
 // Calculate Z from Confidence Level
 fn z_from_cl(cl: f64) -> f64 {
-    erf_inv(cl) * 1.414213562373095
+    erf_inv(cl) * f64::consts::SQRT_2
 }
 
 // Calculate P from Z
@@ -769,69 +802,66 @@ fn p_from_z(z: f64) -> f64 {
 
         if y >= 3.0 {
             x = 1.0;
+        } else if y < 1.0 {
+            w = y * y;
+            x = ((((((((0.000124818987 * w - 0.001075204047) * w + 0.005198775019) * w
+                - 0.019198292004)
+                * w
+                + 0.059054035642)
+                * w
+                - 0.151968751364)
+                * w
+                + 0.319152932694)
+                * w
+                - 0.531923007300)
+                * w
+                + 0.797884560593)
+                * y
+                * 2.0;
         } else {
-            if y < 1.0 {
-                w = y * y;
-                x = ((((((((0.000124818987 * w - 0.001075204047) * w + 0.005198775019) * w
-                    - 0.019198292004)
-                    * w
-                    + 0.059054035642)
-                    * w
-                    - 0.151968751364)
-                    * w
-                    + 0.319152932694)
-                    * w
-                    - 0.531923007300)
-                    * w
-                    + 0.797884560593)
-                    * y
-                    * 2.0;
-            } else {
-                y -= 2.0;
-                x = (((((((((((((-0.000045255659 * y + 0.000152529290) * y
-                    - 0.000019538132)
-                    * y
-                    - 0.000676904986)
-                    * y
-                    + 0.001390604284)
-                    * y
-                    - 0.000794620820)
-                    * y
-                    - 0.002034254874)
-                    * y
-                    + 0.006549791214)
-                    * y
-                    - 0.010557625006)
-                    * y
-                    + 0.011630447319)
-                    * y
-                    - 0.009279453341)
-                    * y
-                    + 0.005353579108)
-                    * y
-                    - 0.002141268741)
-                    * y
-                    + 0.000535310849)
-                    * y
-                    + 0.999936657524;
-            }
+            y -= 2.0;
+            x = (((((((((((((-0.000045255659 * y + 0.000152529290) * y - 0.000019538132)
+                * y
+                - 0.000676904986)
+                * y
+                + 0.001390604284)
+                * y
+                - 0.000794620820)
+                * y
+                - 0.002034254874)
+                * y
+                + 0.006549791214)
+                * y
+                - 0.010557625006)
+                * y
+                + 0.011630447319)
+                * y
+                - 0.009279453341)
+                * y
+                + 0.005353579108)
+                * y
+                - 0.002141268741)
+                * y
+                + 0.000535310849)
+                * y
+                + 0.999936657524;
         }
     }
 
     if z > 0.0 {
-        return (x + 1.0) * 0.5;
+        (x + 1.0) * 0.5
     } else {
-        return (1.0 - x) * 0.5;
+        (1.0 - x) * 0.5
     }
 }
 
 // Pretty Format Scientific Numbers
 fn science_pretty_format(value: f64, digits: usize) -> String {
     if value.abs() == 0.0 {
-        return "0".to_string();
+        "0".to_string();
     }
     if value.abs() >= 10000.0 || value.abs() < 0.001 {
-        return format!("{:.*e}", digits, value).to_string();
+        format!("{:.*e}", digits, value);
     }
     format!("{:.*}", digits, value)
         .trim_end_matches(|c| c == '0')
